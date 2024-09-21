6e7b5904ef5db524a6374c1991af568011bde881
Gianluca Fagherazzi <fagherazzigianluca@gmail.com>
2024-09-21 17:21:55 -0300

    detalles

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { getUserById, getUsers, getUsersPagination } from '../controllers/users.controller.js'
 
 const rutas = Router()
-rutas.get('/paginacion', getUsersPagination)
 rutas.get('/', getUsers)
 rutas.get('/:id', getUserById)
 
