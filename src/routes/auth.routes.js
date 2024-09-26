734ebdd5dce0425a8a8affa85b92d7b351f017b3
Sergio Antozzi <santozzi@gmail.com>
2024-09-25 21:29:11 -0300

    sin auth

--- src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { getAuthLogin } from '../controllers/auth.controller.js'
-
-const rutas = Router()
-rutas.post('/login', getAuthLogin)
-
-export default rutas
