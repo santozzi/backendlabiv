3720835f0cd8d8d5a20af7b81cc807b2bcb6b98f
Sergio Antozzi <santozzi@gmail.com>
2024-09-25 21:30:24 -0300

    sin auth

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { getUserById, getUsers } from '../controllers/users.controller.js'
 
-
 const rutas = Router()
 rutas.get('/', getUsers)
 rutas.get('/:id', getUserById)
