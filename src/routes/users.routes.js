3c01f32d585bad339507a32ec2af857906f522c8
Sergio Antozzi <santozzi@gmail.com>
2024-09-26 00:18:16 -0300

    update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,6 @@ import { authentication } from '../middlewares/auth.middleware.js'
 
 const rutas = Router()
 rutas.get('/', authentication, getUsers)
-rutas.get('/:id', getUserById)
+rutas.get('/:id',authentication, getUserById)
 
 export default rutas
