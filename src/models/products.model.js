7d89b768ada6af1170b0d3d50e9b770fea9e8ed7
Gianluca Fagherazzi <fagherazzigianluca@gmail.com>
2024-09-27 14:45:54 -0300

    correccion de errores

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -20,25 +20,6 @@ const getProductsModel = async (minPrice, maxPrice, page, limit) => {
           const { data } = response
           if ((minPrice === undefined || minPrice === null) && (maxPrice === undefined || maxPrice === null)) {
             result = await paginador(host, data, page, limit)
-          } else if (minPrice === undefined || minPrice === null) {
-            const filtrado = data.filter((product) => {
-              return product.price <= maxPrice
-            })
-            result = await paginador(host, filtrado, page, limit)
-          } else if (maxPrice === undefined || maxPrice === null) {
-            const filtrado = data.filter((product) => {
-              return product.price >= minPrice
-            })
-            result = await paginador(host, filtrado, page, limit)
-          } else if (minPrice > maxPrice) {
-            reject(new InvalidPriceException('El precio mínimo debe ser menor o igual al precio máximo'))
-          } else if (minPrice < 0 || maxPrice < 0) {
-            reject(new InvalidPriceException('El precio debe ser mayor o igual a 0'))
-          } else if (minPrice === maxPrice) {
-            const filtrado = data.filter((product) => {
-              return product.price === minPrice
-            })
-            result = await paginador(host, filtrado, page, limit)
           } else {
             if (isNaN(minPrice) || isNaN(maxPrice)) {
               throw new InvalidPriceException('Los precios deben ser números')
@@ -57,7 +38,7 @@ const getProductsModel = async (minPrice, maxPrice, page, limit) => {
           }
           resolve(result)
         } catch (error) {
-          reject(error.message)
+          reject(error)
         }
       })
       .catch((error) => {
